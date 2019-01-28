8c4de24675019cb329a88f3433c133e6b0449439
atul-bhatt <atulbhatt61@gmail.com>
2019-01-29 05:07:13 +0530

    main file updated with two pages

--- mainFile.py
+++ mainFile.py
@@ -1,22 +1,30 @@
 import tkinter as tk
+from tkinter import ttk
 
 LARGE_FONT = ("Verdana", 12)
 
 
-class SeaofBTCapp(tk.Tk):
+class SeaOfBTCApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
 
+        tk.Tk.iconbitmap(self, default="finance.ico")
+        tk.Tk.wm_title(self, "Bitcoin Trading App")
+
+        container = tk.Frame()
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        # create a dictionary of frames
         self.frames = {}
-        frame = StartPage(container, self)
-        self.frames[StartPage] = frame
-        frame.grid(row=0, column=0, sticky='nsew')
+
+        for F in (StartPage, PageOne, PageTwo):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky='nsew')
+
         self.show_frame(StartPage)
 
     def show_frame(self, cont):
@@ -30,7 +38,35 @@ class StartPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
+        button = ttk.Button(self, text="page one", command=lambda: controller.show_frame(PageOne))
+        button.pack()
+        button1 = ttk.Button(self, text="page two", command=lambda: controller.show_frame(PageTwo))
+        button1.pack()
 
 
-app = SeaofBTCapp()
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page One !!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button = ttk.Button(self, text="start page", command=lambda: controller.show_frame(StartPage))
+        button.pack()
+        button1 = ttk.Button(self, text="page two", command=lambda: controller.show_frame(PageTwo))
+        button1.pack()
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = ttk.Label(self, text="Page Two !!", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+        button = ttk.Button(self, text="start page", command=lambda: controller.show_frame(StartPage))
+        button.pack()
+        button1 = ttk.Button(self, text="page one", command=lambda: controller.show_frame(PageOne))
+        button1.pack()
+
+
+app = SeaOfBTCApp()
 app.mainloop()
